ce71ad0663304460e2b6a17d72e798a0f611eab7
rohit <rohit.vijayakumar@ccs-technologies.com>
2020-01-13 18:02:19 +0530

    initial

---
+++ src/migration/1578917856803-UserMigration.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class UserMigration1578917856803 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+    }
+
+}
